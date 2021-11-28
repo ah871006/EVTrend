f70c45ac22188d265fbd9b1886816e3c357f0e5d
夏淮柔 <judy871006@g.ncu.edu.tw>
2021-11-28 10:06:37 +0800

    新增最新消息頁面

---
+++ EVTrend/Areas/News/Controllers/NewsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EVTrend.Areas.News.Controllers
+{
+    [Area(areaName: "News")]
+
+    public class NewsController : Controller
+    {
+        /// <summary>
+        /// 最新消息View
+        /// </summary>
+        /// <returns></returns>
+
+        public IActionResult Index()
+        {
+            return View("ShowNews");
+        }
+    }
+}
