74bb0f79f1cb9ac83346d33b4c7411bcd91ebb76
夏淮柔 <judy871006@g.ncu.edu.tw>
2021-11-29 13:22:28 +0800

    新增MySQL連線、DB底層設計

--- EVTrend/Controllers/HomeController.cs
+++ EVTrend/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace EVTrend.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : _BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -20,6 +20,15 @@ namespace EVTrend.Controllers
 
         public IActionResult Index()
         {
+            // 測試是否成功撈到資料
+            var sqlStr = string.Format("Select account from member");
+            var data = _DB_GetData(sqlStr);
+
+            if (data.Rows.Count > 0)
+            {
+                ViewData["Account"] = data.Rows[0].ItemArray.GetValue(0).ToString();
+            }
+
             return View();
         }
 
--- EVTrend/Startup.cs
+++ EVTrend/Startup.cs
@@ -15,10 +15,9 @@ namespace EVTrend
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            //ConfigÅª¨ú
+            Helper.ConfigHelperMethods.Config = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -49,8 +48,8 @@ namespace EVTrend
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                   name: "areas",
-                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute(
                     name: "default",
