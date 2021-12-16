46cf1979bbc16128d96dd0adac073f13d75b0aa5
Gina <g3190220@gmail.com>
2021-12-16 17:56:39 +0800

    修復總體呈現資料頁面(原本資料有誤，沒有對齊)、修復總體管理頁面col名稱顛倒

--- EVTrend/Areas/Content/Models/TotalElecModel.cs
+++ EVTrend/Areas/Content/Models/TotalElecModel.cs
@@ -15,15 +15,30 @@ namespace EVTrend.Areas.Content.Models
         public float TotalRegisterNumber { get; set; }
         public float Percentage { get; set; }
 
+
     }
     public class MgtTotalElecModel:TotalElecModel
     {
+        
         public int TotalRegisterNo { get; set; }
         public int YearNo { get; set; }
         public int CountryNo { get; set; }
         public string CreateTime { get; set; }
         public string ModifyTime { get; set; }
 
+    }
+
+    public class TotalDrawModel
+    {
+        public int x { get; set; }
+        public float y { get; set; }
+    }
+
+    //Draw 回傳Model
+    public class DrawDataModel
+    {
+        public List<TotalDrawModel> List_A { get; set; }
+        public List<TotalDrawModel> List_T { get; set; }
     }
 
     //用在年份清單
